dfd103f4a3db299cdb063013f6e3219c155e6078
Erwin Mueller <erwin.mueller@deventm.org>
2013-11-11 14:42:35 +0100

    Add exact value factory

--- src/main/java/com/anrisoftware/globalpom/measurement/ExactAverageValueFactory.java
+++ src/main/java/com/anrisoftware/globalpom/measurement/ExactAverageValueFactory.java
@@ -25,25 +25,6 @@ package com.anrisoftware.globalpom.measurement;
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.9
  */
-public interface ExactAverageValueFactory extends ValueFactory {
+public interface ExactAverageValueFactory extends ExactValueFactory {
 
-	/**
-	 * @see #create(double, ValueFactory)
-	 */
-	Value create(double value);
-
-	/**
-	 * Creates a new exact value that calculates error propagation using simpler
-	 * average errors.
-	 * 
-	 * @param value
-	 *            the value.
-	 * 
-	 * @param valueFactory
-	 *            the {@link ValueFactory} to create values in the mathematical
-	 *            operations.
-	 * 
-	 * @return the exact {@link Value}.
-	 */
-	Value create(double value, ValueFactory valueFactory);
 }
