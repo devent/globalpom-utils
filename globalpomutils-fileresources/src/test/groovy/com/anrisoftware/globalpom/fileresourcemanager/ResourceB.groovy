6f3f632196138cfcfb7508de22e155c573ce2aa8
Erwin Mueller <erwin.mueller@deventm.org>
2014-04-05 17:51:11 +0200

    Split file resources manager in own module.

--- globalpomutils-fileresources/src/test/groovy/com/anrisoftware/globalpom/fileresourcemanager/ResourceB.groovy
+++ globalpomutils-fileresources/src/test/groovy/com/anrisoftware/globalpom/fileresourcemanager/ResourceB.groovy
@@ -1,20 +1,20 @@
 /*
  * Copyright 2013-2014 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of globalpomutils-core.
+ * This file is part of globalpomutils-fileresources.
  *
- * globalpomutils-core is free software: you can redistribute it and/or modify it
+ * globalpomutils-fileresources is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * globalpomutils-core is distributed in the hope that it will be useful, but
+ * globalpomutils-fileresources is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with globalpomutils-core. If not, see <http://www.gnu.org/licenses/>.
+ * along with globalpomutils-fileresources. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.globalpom.fileresourcemanager
 
@@ -26,11 +26,11 @@ package com.anrisoftware.globalpom.fileresourcemanager
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.8
  */
-class ResourceA implements Resource, Serializable {
+class ResourceB implements Resource, Serializable {
 
     @Override
     public String getName() {
-        "A"
+        "B"
     }
 
     @Override
