79d567df367d4f660787efd650375c63da5986e7
Erwin Mueller <erwin.mueller@deventm.org>
2016-01-16 18:51:47 +0100

    Add byte buffer stream

---
+++ globalpomutils-core/src/main/java/com/anrisoftware/globalpom/streams/ByteBufferBackedInputStream.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013-2015 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of globalpomutils-core.
+ *
+ * globalpomutils-core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * globalpomutils-core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with globalpomutils-core. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.globalpom.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * Input stream that reads from a byte buffer. From <a href=
+ * "http://stackoverflow.com/questions/4332264/wrapping-a-bytebuffer-with-an-inputstream"
+ * >Wrapping a ByteBuffer with an InputStream [stackoverflow.com]</a>
+ *
+ *
+ * @author Erwin Müller, erwin.mueller@deventm.de
+ * @since 2.16
+ */
+public class ByteBufferBackedInputStream extends InputStream {
+
+    private final ByteBuffer buf;
+
+    public ByteBufferBackedInputStream(ByteBuffer buf) {
+        this.buf = buf;
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (!buf.hasRemaining()) {
+            return -1;
+        }
+        return buf.get() & 0xFF;
+    }
+
+    @Override
+    public int read(byte[] bytes, int off, int len) throws IOException {
+        if (!buf.hasRemaining()) {
+            return -1;
+        }
+        len = Math.min(len, buf.remaining());
+        buf.get(bytes, off, len);
+        return len;
+    }
+}
