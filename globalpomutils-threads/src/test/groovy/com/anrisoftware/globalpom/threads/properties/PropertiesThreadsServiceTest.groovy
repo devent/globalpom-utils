e5c797cc4c3f0636ffd7185d77c8a00226a97ba0
Erwin Müller <erwin.mueller@deventm.org>
2018-10-06 10:18:58 +0200

    Feature #4056 Updated.

--- globalpomutils-threads/src/test/groovy/com/anrisoftware/globalpom/threads/properties/PropertiesThreadsServiceTest.groovy
+++ globalpomutils-threads/src/test/groovy/com/anrisoftware/globalpom/threads/properties/PropertiesThreadsServiceTest.groovy
@@ -1,30 +1,32 @@
-/*
- * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
- *
+/*-
+ * #%L
+ * Global POM Utilities :: Threads
+ * %%
+ * Copyright (C) 2013 - 2018 Advanced Natural Research Institute
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package com.anrisoftware.globalpom.threads.properties
 
 import static com.anrisoftware.globalpom.utils.TestUtils.*
-import groovy.util.logging.Slf4j
 
 import javax.inject.Inject
 
-import org.apache.sling.testing.mock.osgi.junit.OsgiContext
-import org.junit.Before
-import org.junit.BeforeClass
-import org.junit.Rule
-import org.junit.Test
+import org.apache.sling.testing.mock.osgi.junit5.OsgiContext
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 import com.anrisoftware.globalpom.threads.external.core.Threads
 import com.anrisoftware.globalpom.threads.properties.external.PropertiesThreadsService
@@ -32,6 +34,8 @@ import com.anrisoftware.globalpom.threads.properties.internal.PropertiesThreadsI
 import com.anrisoftware.globalpom.threads.properties.internal.PropertiesThreadsServiceImpl
 import com.anrisoftware.globalpom.utils.TestUtils
 import com.anrisoftware.propertiesutils.ContextPropertiesFactory
+
+import groovy.util.logging.Slf4j
 
 /**
  * @see PropertiesThreadsServiceImpl
@@ -76,8 +80,7 @@ class PropertiesThreadsServiceTest extends AbstractPropertiesThreadsTest {
 
     static Properties properties
 
-    @Rule
-    public final OsgiContext context = new OsgiContext()
+    final OsgiContext context = new OsgiContext()
 
     @Inject
     PropertiesThreadsService service
