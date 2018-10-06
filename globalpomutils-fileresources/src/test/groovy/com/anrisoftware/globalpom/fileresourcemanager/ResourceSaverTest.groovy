805a0387c6d99f8fefc3658b59e7612fd9d695b9
Erwin Müller <erwin.mueller@deventm.org>
2018-10-06 10:58:07 +0200

    Feature #4056 Rework.

--- globalpomutils-fileresources/src/test/groovy/com/anrisoftware/globalpom/fileresourcemanager/ResourceSaverTest.groovy
+++ globalpomutils-fileresources/src/test/groovy/com/anrisoftware/globalpom/fileresourcemanager/ResourceSaverTest.groovy
@@ -1,25 +1,30 @@
-/*
- * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
- *
+/*-
+ * #%L
+ * Global POM Utilities :: File Resources Manager
+ * %%
+ * Copyright (C) 2013 - 2018 Advanced Natural Research Institute
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package com.anrisoftware.globalpom.fileresourcemanager
 
 import org.apache.commons.io.FileUtils
-import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Rule
-import org.junit.Test
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport
 import org.junit.rules.TemporaryFolder
 
 import com.google.inject.Guice
@@ -31,6 +36,7 @@ import com.google.inject.Injector
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.8
  */
+@EnableRuleMigrationSupport
 class ResourceSaverTest {
 
     @Test
