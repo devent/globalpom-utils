b1f016857d91fc3d84e62755c127ec519bd7c4e5
Erwin Müller <erwin.mueller@deventm.org>
2018-10-07 11:25:03 +0200

    Feature #4056 Rework.

--- globalpomutils-reflection/src/test/groovy/com/anrisoftware/globalpom/reflection/annotations/AnnotationDiscoveryTest.groovy
+++ globalpomutils-reflection/src/test/groovy/com/anrisoftware/globalpom/reflection/annotations/AnnotationDiscoveryTest.groovy
@@ -1,25 +1,29 @@
-/*
- * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
- *
+/*-
+ * #%L
+ * Global POM Utilities :: Reflection
+ * %%
+ * Copyright (C) 2014 - 2018 Advanced Natural Research Institute
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package com.anrisoftware.globalpom.reflection.annotations
 
 import java.lang.reflect.Field
 
 import org.apache.commons.lang3.reflect.FieldUtils
-import org.junit.BeforeClass
-import org.junit.Test
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 
 import com.anrisoftware.globalpom.reflection.utils.Bean
 import com.anrisoftware.globalpom.reflection.utils.BeanAnnotation
@@ -28,39 +32,39 @@ import com.google.inject.Injector
 
 /**
  * Tests for {@link AnnotationAccessImpl}.
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.4
  */
 class AnnotationDiscoveryTest extends AnnotationUtils {
 
-	@Test
-	void "find annotations"() {
-		int foundCounter = 0
-		def filter = filterFactory.create([BeanAnnotation])
-		def a = factory.create bean.bean, filter
-		a.addListener([memberFound: { foundCounter++ }]as AnnotationListener)
-		def result = a.call()
-		assert result.size() == 3
-		assert foundCounter == 3
-	}
+    @Test
+    void "find annotations"() {
+        int foundCounter = 0
+        def filter = filterFactory.create([BeanAnnotation])
+        def a = factory.create bean.bean, filter
+        a.addListener([memberFound: { foundCounter++ }]as AnnotationListener)
+        def result = a.call()
+        assert result.size() == 3
+        assert foundCounter == 3
+    }
 
-	static Injector injector
+    static Injector injector
 
-	static AnnotationDiscoveryFactory factory
+    static AnnotationDiscoveryFactory factory
 
-	static AnnotationSetFilterFactory filterFactory
+    static AnnotationSetFilterFactory filterFactory
 
-	static ParentBean bean
+    static ParentBean bean
 
-	static Field field
+    static Field field
 
-	@BeforeAll
-	static void setupFactory() {
-		injector = createInjector()
-		factory = createAnnotationDiscoveryFactory(injector)
-		filterFactory = createAnnotationSetFilterFactory(injector)
-		bean = new ParentBean()
-		field = FieldUtils.getField Bean, "annotatedField", true
-	}
+    @BeforeAll
+    static void setupFactory() {
+        injector = createInjector()
+        factory = createAnnotationDiscoveryFactory(injector)
+        filterFactory = createAnnotationSetFilterFactory(injector)
+        bean = new ParentBean()
+        field = FieldUtils.getField Bean, "annotatedField", true
+    }
 }
--- globalpomutils-reflection/src/test/groovy/com/anrisoftware/globalpom/reflection/beans/BeanFieldTest.groovy
+++ globalpomutils-reflection/src/test/groovy/com/anrisoftware/globalpom/reflection/beans/BeanFieldTest.groovy
@@ -1,63 +1,67 @@
-/*
- * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
- *
+/*-
+ * #%L
+ * Global POM Utilities :: Reflection
+ * %%
+ * Copyright (C) 2014 - 2018 Advanced Natural Research Institute
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package com.anrisoftware.globalpom.reflection.beans
 
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 import com.google.inject.Guice
 import com.google.inject.Injector
 
 /**
  * @see BeanField
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.5
  */
 class BeanFieldTest {
 
-	@Test
-	void "field name for field"() {
-		def object = foo.class.getDeclaredField("fieldFoo")
-		assert beanField.toFieldName(object) == "fieldFoo"
-	}
+    @Test
+    void "field name for field"() {
+        def object = foo.class.getDeclaredField("fieldFoo")
+        assert beanField.toFieldName(object) == "fieldFoo"
+    }
 
-	@Test
-	void "field name for method"() {
-		def object = foo.class.getDeclaredMethod("getFieldBar")
-		assert beanField.toFieldName(object) == "fieldBar"
-	}
+    @Test
+    void "field name for method"() {
+        def object = foo.class.getDeclaredMethod("getFieldBar")
+        assert beanField.toFieldName(object) == "fieldBar"
+    }
 
-	static class Foo {
+    static class Foo {
 
-		public Object fieldFoo
+        public Object fieldFoo
 
-		public Object getFieldBar() {
-		}
-	}
+        public Object getFieldBar() {
+        }
+    }
 
-	Foo foo
+    Foo foo
 
-	BeanField beanField
+    BeanField beanField
 
-	@BeforeEach
-	void createBeanField() {
-		beanField = injector.getInstance(BeanField)
-		foo = new Foo()
-	}
+    @BeforeEach
+    void createBeanField() {
+        beanField = injector.getInstance(BeanField)
+        foo = new Foo()
+    }
 
-	static Injector injector = Guice.createInjector new BeansModule()
+    static Injector injector = Guice.createInjector new BeansModule()
 }
