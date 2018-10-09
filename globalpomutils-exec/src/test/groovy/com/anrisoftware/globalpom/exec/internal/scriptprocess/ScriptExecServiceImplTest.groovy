bc687ed1869f13cbac0cd356885a9ab0a0ed3667
Erwin Müller <erwin.mueller@deventm.org>
2018-10-09 21:37:04 +0200

    Feature #4056 Fix test.

--- globalpomutils-exec/src/test/groovy/com/anrisoftware/globalpom/exec/internal/scriptprocess/ScriptExecServiceImplTest.groovy
+++ globalpomutils-exec/src/test/groovy/com/anrisoftware/globalpom/exec/internal/scriptprocess/ScriptExecServiceImplTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,6 +100,7 @@ class ScriptExecServiceImplTest {
         this.templatesService = context.registerInjectActivateService(new TemplatesServiceImpl(), null)
         this.threadsService = context.registerInjectActivateService(new PropertiesThreadsServiceImpl(), null)
         this.echoScriptTemplate = loadTemplates()
+        createThreadsPool()
     }
 
     static properties
@@ -117,7 +118,6 @@ class ScriptExecServiceImplTest {
         return templates.getResource("echo_script")
     }
 
-    @BeforeEach
     void createThreadsPool() {
         threads = threadsService.create()
         threads.setProperties properties
